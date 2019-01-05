9f2006c8b930c183dbf87762bce5669beb38719c
Marie Carter <marievcarter@gmail.com>
2019-01-05 18:07:41 -0500

    Capitalize module name.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const path = require('path');
-const volleyball = require('volleyball');
+const volleyball = require('Volleyball');
 
 const app = express();
 
