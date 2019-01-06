a608fbe728bf5693c0f81393db6a37521b9d8875
Marie Carter <marievcarter@gmail.com>
2019-01-05 21:27:39 -0500

    Update port.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const { db } = require('./server/db');
 const app = require('./server');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
   .then(() => {
