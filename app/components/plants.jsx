bc1546ee061970fa86477564563331c15e1cda4d
Marie Carter <marievcarter@gmail.com>
2019-01-14 09:36:04 -0500

    Updated selenium tests with time.sleep(5) for demonstration purposes. Also removed page header.

--- app/components/plants.jsx
+++ app/components/plants.jsx
@@ -16,7 +16,7 @@ class Plants extends Component {
   render() {
     return (
       <div>
-        <h1 className="title">Collection</h1>
+        {/* <h1 className="title">Collection</h1> */}
         <NavLink to={'/plants/addPlant'}>
           <button>+ Add Plant</button>
         </NavLink>
--- test.py
+++ test.py
@@ -1,11 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
+# start the web driver
 driver = webdriver.Chrome()
-driver.get("http://localhost:3000/plants")
+# change this to new heroku link
+driver.get("http://plant-track.herokuapp.com/plants")
 
-# buttons we can click on
-driver.find_element(By.LINK_TEXT, "PROFILE").click()
+'''
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "HOME"))
+    )
+finally:
+    driver.quit()
+'''
 
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.LINK_TEXT, "Ponytail Palm"))
+)
+
+time.sleep(5)
+
+# buttons we can click on - is this needed
+driver.find_element(By.LINK_TEXT, "Ponytail Palm").click()
+
+time.sleep(5)
+
+# click on the first plant in the group (maybe ponytail palm)
+
+# WebDriverWait(driver, 1000)
+
+# close the page
 driver.quit()
