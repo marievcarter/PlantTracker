2968dc017e60dd4b22b29f8f469eae1fd7db4992
Marie Carter <marievcarter@gmail.com>
2019-04-21 21:51:21 -0400

    Fix conflicts.

--- app/components/Gallery.jsx
+++ app/components/Gallery.jsx
@@ -3,14 +3,8 @@ import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchPlants, updateDate } from '../reducers/plantReducer';
 import { GalleryItem } from './index.js';
-import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Radio from '@material-ui/core/Radio';
-import Paper from '@material-ui/core/Paper';
 
 const styles = {
   root: {
@@ -31,7 +25,6 @@ class Gallery extends Component {
 
   render() {
     const { classes } = this.props;
-    // const { spacing } = this.state;
     let { plants } = this.props;
     return (
       <div>
